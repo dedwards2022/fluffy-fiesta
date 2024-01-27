9857292f80bd0c0ea7857b05c33050b25fc6cd86
Daniel Edwards <danieledwards@Daniels-MacBook-Pro.local>
2024-01-27 14:09:17 -0700

    Added comments

--- alarmclock.py
+++ alarmclock.py
@@ -4,10 +4,13 @@ current_minute = int(input("Input current minutes (0-59): "))
 current_second = int(input("Input current seconds (0-59):"))
 alarm_set = int(input("Input hours until alarm goes off: "))
 
+# Formats the user inputted hour, minute, second and outputs it to the screen
 time = "{:02d}:{:02d}:{:02d}".format(current_hour, current_minute, current_second)
 print("You inputed:", time)
 
-future_hour = ((current_hour + alarm_set) % 24)
+# Calculates inputted hour to hours until alarm goes off with % 24 (24-hour)
+future_hour = ((current_hour + alarm_set) % 24) 
 
+# Formats outputted alarm clock  and prints it to the screen
 alarm_time = "{:02d}:{:02d}:{:02d}".format(future_hour, current_minute, current_second)
 print("Alarm will go off: ", alarm_time)
